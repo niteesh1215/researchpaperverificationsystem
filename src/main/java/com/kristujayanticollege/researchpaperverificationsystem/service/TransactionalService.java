95d3f4c795f2dfbebf63320836833bfba4114261
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-05-19 00:57:14 +0530

    converted JSONObject to Map

--- src/main/java/com/kristujayanticollege/researchpaperverificationsystem/service/TransactionalService.java
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/service/TransactionalService.java
@@ -1,26 +1,26 @@
 package com.kristujayanticollege.researchpaperverificationsystem.service;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.transaction.Transactional;
 
 import com.kristujayanticollege.researchpaperverificationsystem.model.Upload;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionalService {
     @Transactional
-    public void addRsearchDetails(String json, UploadRepositoryService uploadRepositoryService,
+    public void addRsearchDetails(Map<String,Object> researchDetailsRows, UploadRepositoryService uploadRepositoryService,
             ResearchDetailsRepositoryService researchDetailsRepositoryService,
             ColumnMapRepositoryService columnMapRepositoryService) throws ParseException, java.text.ParseException {
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(json);
-        JSONObject researchDetails = (JSONObject) obj;
-        Upload upload = uploadRepositoryService.save((JSONObject) researchDetails.get("fileDetails"));
-        researchDetailsRepositoryService.addReseachDetails((JSONArray) researchDetails.get("excelData"), upload.getId(),
+        @SuppressWarnings("unchecked")
+        Upload upload = uploadRepositoryService.save((Map<String, Object>) researchDetailsRows.get("fileDetails"));
+        
+        
+        researchDetailsRepositoryService.addReseachDetails((List<Map<String,Object>>) researchDetailsRows.get("excelData"), upload.getId(),
                 columnMapRepositoryService.getAllMapping());
 
     }
