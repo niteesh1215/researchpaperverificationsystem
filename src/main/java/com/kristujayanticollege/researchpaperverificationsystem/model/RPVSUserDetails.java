459184d9882325e5d675b06d3accf1e8be1f50d7
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-06-05 00:03:02 +0530

    implemented spring security

---
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/model/RPVSUserDetails.java
@@ -0,0 +1,60 @@
+package com.kristujayanticollege.researchpaperverificationsystem.model;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class RPVSUserDetails implements UserDetails {
+
+    private User user;
+
+    public RPVSUserDetails() {
+
+    }
+
+    public RPVSUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return Arrays.stream(user.getRoles().split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return user.isActive();
+    }
+
+}
---
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kristujayanticollege.researchpaperverificationsystem.repository;
+
+import java.util.Optional;
+
+import com.kristujayanticollege.researchpaperverificationsystem.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsername(String userName);
+
+    
+}
