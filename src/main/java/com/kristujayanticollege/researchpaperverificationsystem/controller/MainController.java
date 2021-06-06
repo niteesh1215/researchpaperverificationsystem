ab925fdfd1032e223e5bea51aa5302cbb6e7d1c3
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-06-07 01:58:23 +0530

    Moved mappings  to the respective separate controllers

--- src/main/java/com/kristujayanticollege/researchpaperverificationsystem/controller/MainController.java
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/controller/MainController.java
@@ -1,73 +1,43 @@
 package com.kristujayanticollege.researchpaperverificationsystem.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.kristujayanticollege.researchpaperverificationsystem.model.ColumnMap;
-import com.kristujayanticollege.researchpaperverificationsystem.model.ResearchDetailsRow;
-import com.kristujayanticollege.researchpaperverificationsystem.model.Upload;
-import com.kristujayanticollege.researchpaperverificationsystem.model.User;
-import com.kristujayanticollege.researchpaperverificationsystem.model.VerificationDetails;
-import com.kristujayanticollege.researchpaperverificationsystem.repository.UserRepository;
-import com.kristujayanticollege.researchpaperverificationsystem.service.other.TransactionalService;
-import com.kristujayanticollege.researchpaperverificationsystem.service.repository.ColumnMapRepositoryService;
+import javax.servlet.http.HttpServletRequest;
+
 import com.kristujayanticollege.researchpaperverificationsystem.service.repository.ResearchDetailsRepositoryService;
 import com.kristujayanticollege.researchpaperverificationsystem.service.repository.UploadRepositoryService;
-import com.kristujayanticollege.researchpaperverificationsystem.service.repository.VerificationDetailsRepositoryService;
-import com.kristujayanticollege.researchpaperverificationsystem.service.verification.PeerReviewedVerificationService;
-import com.kristujayanticollege.researchpaperverificationsystem.service.verification.ScopusVerificationService;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class MainController {
+public class MainController implements ErrorController {
 
 	@Autowired
 	private ResearchDetailsRepositoryService researchDetailsRepositoryService;
 
 	@Autowired
-	private ColumnMapRepositoryService columnMapRepositoryService;
-
-	@Autowired
 	private UploadRepositoryService uploadRepositoryService;
-
-	@Autowired
-	private VerificationDetailsRepositoryService verificationRepositoryService;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
 	WebClient.Builder webClientBuilder;
 
-	@Value("${scopus.api.key.value}")
-	private String apiKey;
-
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	@GetMapping(path = "/login")
-	public ModelAndView login() {
+	public ModelAndView loginView(HttpServletRequest request) {
+		try {
+			String referrer = request.getHeader("Referer");
+			if (referrer != null)
+				request.getSession().setAttribute("url_prior_login", referrer);
+		} catch (Exception e) {
+		}
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("view/login");
 		return modelAndView;
@@ -113,7 +83,8 @@ public class MainController {
 			map.put("uploads_count", uploadRepositoryService.getTotalUploads());
 			map.put("total_entries", researchDetailsRepositoryService.getCount());
 			map.put("year_wise_count", researchDetailsRepositoryService.getGroupByYearWiseCount());
-			map.put("indexing_wise_verification_status_count", researchDetailsRepositoryService.getGroupByVerificationStatusAndIndexingCount());
+			map.put("indexing_wise_verification_status_count",
+					researchDetailsRepositoryService.getGroupByVerificationStatusAndIndexingCount());
 			map.put("status", "success");
 		} catch (Exception e) {
 			map.clear();
@@ -124,319 +95,15 @@ public class MainController {
 		return map;
 	}
 
-	@PostMapping(path = "/save")
-	public ResponseEntity<HashMap<String, String>> save(@RequestBody Map<String, Object> researchDetailsRows) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			TransactionalService ts = new TransactionalService();
-			ts.addRsearchDetails(researchDetailsRows, uploadRepositoryService, researchDetailsRepositoryService,
-					columnMapRepositoryService);
-			map.put("status", "success");
-			map.put("message", "Successfuly added");
-			return ResponseEntity.status(HttpStatus.OK).body(map);
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			if (e.toString().contains("UNIQUE")) {
-				map.put("message", "file name exists");
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
-			} else {
-				map.put("message", "An unknown error occurred");
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
-			}
-		}
+	
+
+	@RequestMapping(value = "/error")
+	public String error() {
+		return "An error occurred";
 	}
 
-	@GetMapping(path = "get-research-details/{groupId}")
-	public @ResponseBody List<Map<String, Object>> getResearchDetailsByGroupId(@PathVariable Long groupId) {
-		try {
-			return researchDetailsRepositoryService.getResearchDetailsByGroupId(groupId);
-		} catch (Exception e) {
-			System.out.println(e);
-			return new ArrayList<Map<String, Object>>();
-		}
-	}
-
-	@PostMapping(path = "/add-new-row")
-	public @ResponseBody HashMap<String, String> addNewRow(@RequestBody Map<String, Object> newRow) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			researchDetailsRepositoryService.addNewResearchDetailsRow(newRow,
-					columnMapRepositoryService.getAllMapping());
-			map.put("status", "success");
-			map.put("message", "Successfuly deleted");
-			return map;
-		} catch (Exception e) {
-			System.out.println(e);
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return map;
-		}
-	}
-
-	@PostMapping(path = "delete-rows")
-	public @ResponseBody HashMap<String, String> deleteResearchDetailsRow(@RequestBody List<Long> ids) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-
-			if (ids == null || ids.isEmpty()) {
-				map.put("status", "invalid_request");
-				map.put("message", "empty list");
-				return map;
-			}
-
-			for (int i = 0; i < ids.size(); i++) {
-				researchDetailsRepositoryService.deleteResearchDetailsById(ids.get(i));
-			}
-
-			map.put("status", "success");
-			map.put("message", "Successfuly deleted");
-			return map;
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return map;
-		}
-	}
-
-	@PostMapping(path = "/update-row")
-	public @ResponseBody HashMap<String, String> updateRow(@RequestBody Map<String, Object> editedRow) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			researchDetailsRepositoryService.updateResearchDetailsRow(editedRow,
-					columnMapRepositoryService.getAllMapping());
-			map.put("status", "success");
-			map.put("message", "Successfuly deleted");
-			return map;
-		} catch (Exception e) {
-			System.out.println(e);
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return map;
-		}
-	}
-
-	@GetMapping(path = "/columns-mapping")
-	public @ResponseBody Iterable<ColumnMap> columnsMapping() {
-		return columnMapRepositoryService.getAllMapping();
-	}
-
-	@PostMapping(path = "/add-new-column-mapping")
-	public ResponseEntity<HashMap<String, String>> addNewMapping(@RequestBody ColumnMap columnMap) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			columnMapRepositoryService.addNewMapping(columnMap);
-			map.put("status", "success");
-			map.put("message", "Successfuly added");
-			return ResponseEntity.status(HttpStatus.OK).body(map);
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			if (e.toString().contains("UNIQUE")) {
-				map.put("message", "column/mapped name exists");
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
-			} else {
-				map.put("message", "An unknown error occurred");
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
-			}
-		}
-	}
-
-	@GetMapping("/delete-column-mapping/{id}")
-	public ResponseEntity<HashMap<String, String>> deleteColumnMapping(@PathVariable Long id) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			columnMapRepositoryService.deleteMappingById(id);
-			map.put("status", "success");
-			map.put("message", "Successfuly deleted");
-			return new ResponseEntity<HashMap<String, String>>(map, HttpStatus.OK);
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return new ResponseEntity<HashMap<String, String>>(map, HttpStatus.NOT_FOUND);
-		}
-	}
-
-	@GetMapping(path = "/get-uploads")
-	public @ResponseBody Iterable<Upload> getUploads() {
-		return uploadRepositoryService.getAllUploads();
-	}
-
-	@GetMapping("/delete-upload/{id}")
-	public ResponseEntity<HashMap<String, String>> deleteUpload(@PathVariable Long id) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			uploadRepositoryService.deleteUploadById(id);
-			map.put("status", "success");
-			map.put("message", "Successfuly deleted");
-			return new ResponseEntity<HashMap<String, String>>(map, HttpStatus.OK);
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return new ResponseEntity<HashMap<String, String>>(map, HttpStatus.NOT_FOUND);
-		}
-	}
-
-	@GetMapping(path = "/verify-file/{id}")
-	public @ResponseBody HashMap<String, String> verify(@PathVariable Long id) {
-
-		System.out.println(id);
-		HashMap<String, String> map = new HashMap<>();
-
-		try {
-			List<Map<String, Object>> researchDetailsList = researchDetailsRepositoryService
-					.getResearchDetailsByGroupId(id);
-
-			if (researchDetailsList.isEmpty()) {
-				map.put("status", "not_found");
-				map.put("message", "id not found");
-				return map;
-			}
-
-			List<ResearchDetailsRow> researchDetailsRows = objectMapper.convertValue(researchDetailsList,
-					new TypeReference<List<ResearchDetailsRow>>() {
-					});
-
-			ScopusVerificationService scopusVerificationService = new ScopusVerificationService();
-			scopusVerificationService.setWebClientBuilder(webClientBuilder);
-			scopusVerificationService.setObjectMapper(objectMapper);
-
-			PeerReviewedVerificationService peerReviewedVerificationService = new PeerReviewedVerificationService();
-
-			if (researchDetailsRows != null) {
-				for (int i = 0; i < researchDetailsRows.size(); i++) {
-
-					try {
-						if ((researchDetailsRows.get(i).getIndexing() != null))
-							if (researchDetailsRows.get(i).getIndexing().equalsIgnoreCase("SCOPUS")) {
-
-								scopusVerificationService.setApiKey(apiKey);
-								scopusVerificationService.setResearchDetailsRow(researchDetailsRows.get(i));
-								scopusVerificationService.verify();
-								scopusVerificationService.saveStatus(verificationRepositoryService,
-										researchDetailsRepositoryService);
-
-							} else if (researchDetailsRows.get(i).getIndexing()
-									.equalsIgnoreCase("Refereed / Peer Reviewed")) {
-
-								peerReviewedVerificationService.setResearchDetailsRow(researchDetailsRows.get(i));
-								peerReviewedVerificationService.verify();
-								System.out.println(peerReviewedVerificationService.foundUrls);
-								peerReviewedVerificationService.saveStatus(verificationRepositoryService,
-										researchDetailsRepositoryService);
-							}
-
-					} catch (Exception e) {
-						System.out.println(">>>>>>>>>>>>>>>>>>>>>>" + researchDetailsRows.get(i).getId());
-					}
-				}
-
-			}
-
-			map.put("status", "success");
-			map.put("message", "verification successful");
-			return map;
-		} catch (Exception e) {
-			System.out.println(e);
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return map;
-		}
-
-	}
-
-	@PostMapping(path = "/verify-rows")
-	public @ResponseBody HashMap<String, String> verifyRow(@RequestBody List<Long> ids) {
-
-		HashMap<String, String> map = new HashMap<>();
-
-		// TODO: add for different indexing
-		try {
-
-			if (ids == null || ids.isEmpty()) {
-				map.put("status", "invalid_request");
-				map.put("message", "empty list");
-				return map;
-			}
-
-			ScopusVerificationService service = new ScopusVerificationService();
-			service.setWebClientBuilder(webClientBuilder);
-			service.setObjectMapper(objectMapper);
-
-			PeerReviewedVerificationService peerReviewedVerificationService = new PeerReviewedVerificationService();
-
-			for (int i = 0; i < ids.size(); i++) {
-				Map<String, Object> researchDetails = researchDetailsRepositoryService
-						.getResearchDetailsById(ids.get(i));
-
-				if (researchDetails == null)
-					continue;
-				ResearchDetailsRow row = objectMapper.convertValue(researchDetails, ResearchDetailsRow.class);
-
-				if (row.getIndexing() != null)
-					if (row.getIndexing().equalsIgnoreCase("SCOPUS")) {
-						service.setApiKey(apiKey);
-						service.setResearchDetailsRow(row);
-						service.verify();
-						service.saveStatus(verificationRepositoryService, researchDetailsRepositoryService);
-					} else if (row.getIndexing().equalsIgnoreCase("Refereed / Peer Reviewed")) {
-
-						peerReviewedVerificationService.setResearchDetailsRow(row);
-						peerReviewedVerificationService.verify();
-						System.out.println(peerReviewedVerificationService.foundUrls);
-						peerReviewedVerificationService.saveStatus(verificationRepositoryService,
-								researchDetailsRepositoryService);
-					}
-				Thread.sleep(5000);
-
-			}
-
-			map.put("status", "success");
-			map.put("message", "verification successful");
-			return map;
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			map.put("message", "An unknown error occurred");
-			return map;
-		}
-
-	}
-
-	@GetMapping(path = "verification-details/{id}")
-	public @ResponseBody VerificationDetails getVerificationDetailsById(@PathVariable Long id) {
-		Optional<VerificationDetails> details = verificationRepositoryService.getVerificationDetailsById(id);
-		if (details.isPresent())
-			return details.get();
-		else
-			return new VerificationDetails();
-	}
-
-	@PostMapping(path="/add-new-user")
-	public @ResponseBody HashMap<String, String> addNewUser(@RequestBody User user) {
-		HashMap<String, String> map = new HashMap<>();
-		try {
-			user.setPassword(passwordEncoder.encode(user.getPassword()));
-			userRepository.save(user);
-			map.put("status", "success");
-			map.put("message", "Successfuly added");
-			return map;
-		} catch (Exception e) {
-			map.clear();
-			map.put("status", "error");
-			if (e.toString().contains("UNIQUE")) {
-				map.put("message", "username exists");
-				return map;
-			} else {
-				map.put("message", "An unknown error occurred");
-				return map;
-			}
-		}
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 }
