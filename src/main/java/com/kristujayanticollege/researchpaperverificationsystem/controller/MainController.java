e2135859c264716e91f9722c0d35d4cad507321c
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-07-12 13:00:47 +0530

    added support for filters in dashboard-details mapping

--- src/main/java/com/kristujayanticollege/researchpaperverificationsystem/controller/MainController.java
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/controller/MainController.java
@@ -13,6 +13,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,7 +30,6 @@ public class MainController implements ErrorController {
 
 	@Autowired
 	WebClient.Builder webClientBuilder;
-
 
 	@GetMapping(path = "/login")
 	public ModelAndView loginView(HttpServletRequest request) {
@@ -71,11 +72,20 @@ public class MainController implements ErrorController {
 		return modelAndView;
 	}
 
-	@GetMapping(path = "/dashboard-details")
-	public @ResponseBody Map<String, Object> dashboardDetails() {
+	@PostMapping(path = "/dashboard-details")
+	public @ResponseBody Map<String, Object> dashboardDetails(@RequestBody Map<String, Object> filters) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		// researchDetailsRepositoryService.getGroupByIndexingCount();
+
 		try {
+
+			if (filters == null)
+				researchDetailsRepositoryService.setFilters(null);
+			else if (filters.isEmpty())
+				researchDetailsRepositoryService.setFilters(null);
+			else
+				researchDetailsRepositoryService.setFilters(filters);
+
 			map.put("indexing_count_list", researchDetailsRepositoryService.getGroupByIndexingCount());
 			map.put("department_count_list", researchDetailsRepositoryService.getGroupByDepartmentCount());
 			map.put("verification_status_count_list",
@@ -85,17 +95,23 @@ public class MainController implements ErrorController {
 			map.put("year_wise_count", researchDetailsRepositoryService.getGroupByYearWiseCount());
 			map.put("indexing_wise_verification_status_count",
 					researchDetailsRepositoryService.getGroupByVerificationStatusAndIndexingCount());
+			map.put("year", researchDetailsRepositoryService.getDistinctYears());
 			map.put("status", "success");
+
 		} catch (Exception e) {
+
+			System.out.println(e);
+
 			map.clear();
 			map.put("status", "error");
 			map.put("message", "An unknown error occurred");
 		}
 
+		// resetting the filters for next request
+		researchDetailsRepositoryService.setFilters(null);
+
 		return map;
 	}
-
-	
 
 	@RequestMapping(value = "/error")
 	public String error() {
