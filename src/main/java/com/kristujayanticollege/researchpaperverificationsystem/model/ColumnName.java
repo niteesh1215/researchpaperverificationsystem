a5c9f1e841f64093b9b74e11b2c491c65a4cee5d
Niteesh Mahato <niteeshmahato15@gmail.com>
2021-07-19 21:29:02 +0530

    column name static variable

---
+++ src/main/java/com/kristujayanticollege/researchpaperverificationsystem/model/ColumnName.java
@@ -0,0 +1,12 @@
+package com.kristujayanticollege.researchpaperverificationsystem.model;
+
+public class ColumnName {
+    //public static final String manuscriptTitle = "manuscript_title";
+
+    public static final String manuscriptTitle= "Manuscript Title";
+    //public static final String fullName = "name";
+    public static final String fullName ="Name [Full Name without Fr. / Dr. / Mr. / Ms. ]";
+
+    //public static final String journalTitle = "journal_title";
+    public static final String journalTitle = "Journal Title";
+}
